e9912a4aa518a48d7f19aa15ffa1a5e1ef65fcf7
Gertjan Reynaert <gertjan@typework.com>
2016-01-14 09:22:11 +0100

    [ci skip] wip

---
+++ test/getLanguageReport.test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+import getLanguageReport from '~/src/getLanguageReport';
+
+describe('getLanguageReport', () => {
+  it('should give back the full report for a language', () => {
+    const actual = {};
+    const expected = {};
+    expect(actual).to.deep.equal(expected);
+  });
+});
