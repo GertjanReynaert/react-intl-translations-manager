438041ab58c88fe913e92dc722063bcaffb357e6
Gertjan Reynaert <gertjan@typework.com>
2016-12-16 19:53:51 +0100

    refactor(Core): Remove Core from exposed api
    BREAKING CHANGE: Since the full potential of core is now exposed through the default
    translationManager, it is no longer necessary to expose this method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 export { default } from './manageTranslations';
 
-export { default as core } from './core';
 export { default as readMessageFiles } from './readMessageFiles';
 export { default as createSingleMessagesFile } from './createSingleMessagesFile';
 export { default as getDefaultMessages } from './getDefaultMessages';
